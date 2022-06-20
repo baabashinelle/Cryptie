e033eebece4ec3343eb14845b613605d9f66ff22
Baaba <baabadampare@gmail.com>
2022-06-20 21:24:15 +0000

    cursor changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
             className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
             flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
           >
-            <li className="text-xl w-full my-2">
+            <li className="text-xl w-full my-2 cursor-pointer">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
             {["Buy/Sell", "Grow", "Wallets", "Market"].map((item, index) => (
