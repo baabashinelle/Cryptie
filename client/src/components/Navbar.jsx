1004fdb8916c3dbb53a3a3cdb0610973c38a4347
Baaba <baabadampare@gmail.com>
2022-07-16 00:49:14 +0000

    edit mobile menu alignment

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -48,26 +48,32 @@ const Navbar = () => {
         )}
         {toggleMenu && (
           <ul
-            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+            className="z-10 fixed -top-0 -left-0 p-3 w-full h-screen shadow-2xl md:hidden list-none
+            flex flex-col justify-start items-center rounded-md bg-[#25335c] text-white animate-slide-in"
           >
-            <li className="text-xl w-full my-2 cursor-pointer">
+            <li className="text-2xl w-full flex justify-end my-2 cursor-pointer pb-10">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
             {["Buy/Sell", "Grow", "Wallets", "Market"].map((item, index) => (
               <NavbarItem
                 key={item + index}
                 title={item}
-                classProps="my-2 text-lg pt-3"
+                classProps="my-2 text-lg pt-5"
               />
             ))}
-            <div className="flex flex-col-reverse gap-5 list-none text-white">
+            <div className="flex flex-col-reverse gap-7 list-none text-white pt-2">
               <li className="bg-transparent border-[1px] py-2 px-7  rounded-lg cursor-pointer hover:bg-[#2952e3] hover:border-[#2952e3]">
                 Sign Up
               </li>
               <li className="bg-[#2952e3] border-[1px] border-[#2952e3] py-2 px-7 mx-1 rounded-lg cursor-pointer hover:bg-transparent hover:border-[#fff] mt-3">
                 Sign In
               </li>
+            </div>
+
+            <div className="pt-20 flex gap-6 text-[#ffffff99] text-sm">
+              <span>Instagram</span>
+              <span>Facebook</span>
+              <span>Twitter</span>
             </div>
           </ul>
         )}
