dd416d9b13a2762011c44918b8bc0ab66ae12834
Baaba <baabadampare@gmail.com>
2022-07-16 01:05:11 +0000

    edit tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
   },
   plugins: [],
 };
